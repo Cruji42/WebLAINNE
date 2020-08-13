54321b0dcc76681e2170ca2f036a6b2b8c5eb4cd
Cruji42 <l.move971223@itses.edu.mx>
2020-08-12 23:17:08 -0500

    Before merge

--- src/app/layout/layout-routing.module.ts
+++ src/app/layout/layout-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {LayoutComponent} from './layout.component';
+import {LandingPageComponent} from '../landing-page/landing-page.component';
 
 
 const routes: Routes = [{
@@ -9,7 +10,8 @@ const routes: Routes = [{
     {path: '', redirectTo: 'cakemaker', pathMatch: 'prefix'},
     {path: 'cakemaker', loadChildren: () => import('./cakemaker/cakemaker.module').then(
         (m) => m.CakemakerModule
-      )}
+      )},
+    { path: 'home', component: LandingPageComponent },
   ]
 }];
 
